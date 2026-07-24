b2b0c44d6da508eb6fa3be12debc07f556e6a9a9
jeyoungyu <dbwpdud85@gmail.com>
2026-07-24 10:59:12 +0900

    first commit

---
+++ src/App.java
@@ -0,0 +1,22 @@
+
+public class App {
+
+    // 한줄주석
+    /*
+        여러줄 주석
+    */
+
+   /**
+    * 프로그램 주석
+    * @param args
+    */
+
+
+   // main 메서드 :프로그램 실행
+   //  메서드의 선언부
+   // 접근제한자 반환타입 메서드이름(매개변수타입 매개변수이름)
+   public static void main(String[] args) {
+        //자바의 표준 출력
+        System.out.println("hello world !");
+   }
+}
